a6c792c909aaeb0e05755c1f1580ec35d182ef92
santiagomoreta <santi_ago_m@gmail.com>
2015-01-30 08:59:19 +0100

    desd netbeans

--- server/routes/user.js
+++ server/routes/user.js
@@ -109,63 +109,113 @@ var requestify = require('requestify');
       res.send(user);
     
   };
-  //POST - Insert a new GoogleUser in the DB
   
-  addGoogleUser = function(req, res) {
+  
+  //------------------------xploit-----------------------------------
+  
+   addXploit = function(req, res) {
         console.log('POST - /user');
         //-------------------
         res.header('Access-Control-Allow-Origin', "*");     // TODO - Make this more secure!!
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST');
         res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept');
         //----------------------------------------
-    console.log(req.body+"token------id");
-    var url='https://www.googleapis.com/plus/v1/people/'+req.body.id+'?access_token='+req.body.token;
-    console.log("url----------->"+url);
+    console.log(req.body);
+ 
+   /* var user = new User({
+                name: req.body.name,
+                email :req.body.email, 
+                password : req.body.password
+    });
+ 
+    user.save(function(err) {
+      if(!err) {
+        console.log("User created");
+        res.send({ status: 'OK', user:user });
+      } else {
+        console.log(err);
+        if(err.name === 'ValidationError') {
+          res.statusCode = 400;
+          res.send({ error: 'Validation error' });
+        } else {
+          res.statusCode = 500;
+          res.send({ error: 'Server error' });
+        }
+        console.log('Internal error(%d): %s',res.statusCode,err.message);
+      }
+    });*/
+ 
+  //revisar
+     res.redirect('your/404/path.html');
+    
+  };
+  
+  
+  
+  
+  
+  
+  
+  
+  //POST - Insert a new GoogleUser in the DB
+  
+  addGoogleUser = function(req, res) {
+        console.log('POST - /user');
+       
+    console.log(req.body.token+"token------id");
+      
+    var url='https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token='+req.body.token;
+    
                 requestify.get(url).then(function(response) {
                     
                     //response.getBody();
                     console.log("--------------->"+response.body+" ----  bosy");
-                    var jsonconverter=JSON.parse(response.body);
-                    console.log(jsonconverter.id+"----------->json");
-                    
-                    
-         User.findOne({email: jsonconverter.emails[0].value}, function(err,user) { 
+                  var body=JSON.parse(response.body);
+                   // console.log(jsonconverter.id+"----------->json");
+                   
+                    console.log("--------------->"+body.email+" ----  bosy");
+                     User.findOne({email:body.email }, function(err,user) { 
 
                             if(!user) {
-                           var user = new User({
-                            name: jsonconverter.displayName,
-                            email :jsonconverter.emails[0].value, 
-                            password : jsonconverter.id
+                                var user = new User({
+                                 name: body.name,
+                                 email :body.email, 
+                                 password :body.id
 
-                            });
-                            console.log("//////////------------>"+user+" ////-->user created------------");
-                            user.save(function(err) {
-                              if(!err) {
-                                console.log("User created");
-                                res.send({ status: 'OK', user:user });
-                              } else {
-                                console.log(err);
-                                if(err.name === 'ValidationError') {
-                                  res.statusCode = 400;
-                                  res.send({ error: 'Validation error' });
-                                } else {
-                                  res.statusCode = 500;
-                                  res.send({ error: 'Server error' });
-                                }
-                                console.log('Internal error(%d): %s',res.statusCode,err.message);
-                              }
-                            });
-
-                          //revisar
-                              
-   
+                                 });
+                                 console.log("//////////------------>"+user+" ////-->user created------------");
+                                 user.save(function(err) {
+                                   if(!err) {
+                                     console.log("User created");
+                                     res.send("usuario añadido a la bd");
+                                   } else {
+                                     console.log(err);
+                                     if(err.name === 'ValidationError') {
+                                       res.statusCode = 400;
+                                       res.send({ error: 'Validation error' });
+                                     } else {
+                                       res.statusCode = 500;
+                                       res.send({ error: 'Server error' });
+                                     }
+                                     console.log('Internal error(%d): %s',res.statusCode,err.message);
+                                   }
+                                 });
+                                 
                             
-                            }else{
-                            res.send({name:jsonconverter.displayName ,email:jsonconverter.emails[0].value, password:jsonconverter.id}); 
-                                
                             }
+                                    
+                                    
+                                
+                            
                             if(!err) {
-                             res.send({ status: 'OK', user:user });
+                             res.send(
+                                {
+                                   name:body.name,
+                                   email :body.email, 
+                                   id:body.id, 
+                                   image:body.picture
+
+                                   }   );
                             } else 
                             {
                               
@@ -256,6 +306,7 @@ var requestify = require('requestify');
   app.get('/user/:email', findByEmail);
   app.post('/googleUser', addGoogleUser);
   app.post('/user', addUser);
+  app.post('/userXploit',addXploit);
   
   app.put('/user/:email', updateUser);
   app.delete('/user/:email', deleteUser);
