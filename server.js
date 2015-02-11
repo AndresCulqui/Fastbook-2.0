7d996537c78260b6f9d3978e9acc1bbf2dfa67f2
Santiago Moreta <santiago.moreta.c@gmail.com>
2015-02-11 12:07:22 +0100

    socket io paper toast ok

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // get all the tools we need
 var express  = require('express');
 var app      = express();
+var http = require('http').Server(app);
 var port     = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var passport = require('passport');
@@ -13,6 +14,7 @@ var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
+var io = require('socket.io')(http);
 
 var configDB = require('./server/config/database.js');
 
@@ -28,6 +30,7 @@ app.use(bodyParser.json()); // get information from html forms
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
+
 //app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
@@ -40,8 +43,24 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 require('./server/routes/book.js')(app);
 require('./server/routes/user.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
+
+//----------------------------------socket------------------------
+//socket io
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log(msg);
+    io.emit('chat message', msg);
+
+  });
+});
+
 // launch ======================================================================
-app.listen(port);
-console.log('The magic happens on port ' + port);
+/*app.listen(port);
+console.log('The magic happens on port ' + port);*/
+
+http.listen(port, function(){
+  console.log('listening on *:' +port);
+});
+
 
 module.exports = app;
